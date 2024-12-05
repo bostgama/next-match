2453bdc512c8629794d048fa65deecc05c2fe6b7
Jose <bostgama@gmail.com>
2024-12-05 15:24:44 -0500

    End of section 4

---
+++ src/app/actions/memberActions.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
+import { Photo } from "@prisma/client";
+
+export async function getMembers() {
+    const session = await auth();
+
+    if (!session?.user) return null;
+
+    try {
+         throw new Error ("error");
+
+        return prisma.member.findMany({
+            where: {
+                NOT: {
+                    userId: session.user.id
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        throw new Error;
+    }
+}
+
+export async function getMemberByUserId(userId: string) {
+    try {
+        return prisma.member.findUnique({where: {userId}})
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getMemberPhotosByUserId(userId: string) {
+
+    const member = await prisma.member.findUnique({
+        where: {userId},
+        select: {photos: true}
+    });
+
+    if(!member) return null;
+
+    return member.photos.map(p => p) as Photo[];
+}
